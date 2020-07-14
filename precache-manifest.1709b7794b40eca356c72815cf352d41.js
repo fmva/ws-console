d2dccc995fd77b817b20d5c86903500c4d627716
Vladimir <fmva@ngs.ru>
2020-07-14 16:03:30 +0700

    Updates

---
+++ precache-manifest.1709b7794b40eca356c72815cf352d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd934a2b63cdd8b40079b5cc26ba36e2",
+    "url": "/ws-console/index.html"
+  },
+  {
+    "revision": "973f4d5cc3f78ece8413",
+    "url": "/ws-console/static/css/main.976783fa.chunk.css"
+  },
+  {
+    "revision": "697884967458f50bc651",
+    "url": "/ws-console/static/js/2.78d5dbb7.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/ws-console/static/js/2.78d5dbb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973f4d5cc3f78ece8413",
+    "url": "/ws-console/static/js/main.e4794ebd.chunk.js"
+  },
+  {
+    "revision": "af06110d96cbd588a337",
+    "url": "/ws-console/static/js/runtime-main.200f4ff3.js"
+  }
+]);
